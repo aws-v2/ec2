b2a6a928f9fd602350cf01b39a8a5cb0bdde797c
Qarani-m <emqarani@gmail.com>
2026-05-17 12:29:05 +0300

    fix: include payload URL in s3 pre-flight failure error message

--- migrations/017_add_ssh_private_key_to_hosts.sql
+++ migrations/017_add_ssh_private_key_to_hosts.sql
@@ -1,6 +1,5 @@
 -- migration 017: Add ssh_private_key column to hosts table
 ALTER TABLE hosts ADD COLUMN IF NOT EXISTS ssh_private_key TEXT;
--- ALTER TABLE vpcs DROP CONSTRAINT vpcs_cidr_block_key;
 
 
 
